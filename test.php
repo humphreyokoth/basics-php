b433d27641257c12fd7e8a7a8b312cb6207a9426
humphreyokoth <humpasp@gmail.com>
2022-09-01 05:59:29 +0300

    Feat:PHP BASICS

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php
+    echo "Hello World";
+    
+    $txt = "Hello World";
+    echo  $txt;
+
+    
+    function myTest(){
+        $x=5;
+        echo "<p> Variable x inside function is:$x</p>";
+    }
+    myTest();
+    ?></h1>
+</body>
+</html>
