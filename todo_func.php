61274b4cee87e15c60e32a8f7af5db0e4b6112e0
humphreyokoth <humpasp@gmail.com>
2022-09-03 09:44:54 +0300

    Feat:define Constant and Global variable functions

--- todo_func.php
+++ todo_func.php
@@ -16,3 +16,20 @@ function todo(){
  echo todo();
  
 ?>
+<?php 
+
+function setHeight($minheight = 50) {
+
+  echo "The height is : $minheight <br>";
+
+}
+
+setHeight(350);
+
+setHeight(); // will use the default value of 50
+
+setHeight(135);
+
+setHeight(80);
+
+?>
