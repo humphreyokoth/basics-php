0cb808691b970fdf93cba3509e99c78bb8c14a0d
humphreyokoth <humpasp@gmail.com>
2022-09-05 07:21:32 +0300

    Feat:Loops

--- todo_func.php
+++ todo_func.php
@@ -36,5 +36,32 @@ $name_length = strlen($name);
 for ($i=0; $i <$name_length ; $i++) { 
   echo $i;
 }
+$employees = array("john","micheal","Mari","Luke","Nellie");
+foreach($employees as $employee){
+echo $employee;
+}
 
+$newarr[$newkey] = $oldarr[$oldkey];
+$oldarr=$newarr;
+unset($newarr);
+$array = [
+  'old1' => 1,
+  'old2' => 2
+];
+
+$renameMap = [
+  'old1' => 'new1',   
+  'old2' => 'new2'
+];
+
+$array = array_combine(array_map(function($el) use ($renameMap) {
+  return $renameMap[$el];
+}, array_keys($array)), array_values($array));
+
+/*
+$array = [
+  'new1' => 1
+  'new2' => 2
+];
+*/
 ?>
