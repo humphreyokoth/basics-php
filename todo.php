1412f8e9940e8e9e35a1621d3069a7564f95e089
humphreyokoth <humpasp@gmail.com>
2022-09-06 07:06:48 +0300

    Feat:Global variable

--- todo.php
+++ todo.php
@@ -1,18 +1,18 @@
 <?php
 // Declaring global variables of a todoList.
 
-//$GLOBALS['to_do_list'] = ["shopping","cleaning","reading","praying","swimming"]; 
-define('to_do_list',["shopping", "cleaning", "reading", "praying", "swimming"]);
+$GLOBALS['to_do_list'] = ["shopping","cleaning","reading","praying","swimming"]; 
+//define('to_do_list',["shopping", "cleaning", "reading", "praying", "swimming"]);
 
 
 //Returning list of items in the global varaible.
 function todo($item){
-  foreach(to_do_list as $item){
-   echo "$item \t";
+  foreach($GLOBALS['to_do_list'] as $item){
+   echo "$item \n";
    }
    
  }
- echo todo('to_do_list');
+todo('to_do_list');
 ?>
 
 
